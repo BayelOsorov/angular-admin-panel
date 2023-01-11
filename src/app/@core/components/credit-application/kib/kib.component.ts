57e284169015c1d7f4a4d2ff443162332850044d
BayelOsorov <osorov.bayel@gmail.com>
2023-01-11 17:56:14 +0600

    done kib

--- src/app/@core/components/credit-application/kib/kib.component.ts
+++ src/app/@core/components/credit-application/kib/kib.component.ts
@@ -1,156 +1,50 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NbMomentDateService } from '@nebular/moment';
 import {
     NbSortDirection,
     NbSortRequest,
     NbTreeGridDataSource,
     NbTreeGridDataSourceBuilder,
 } from '@nebular/theme';
-
-interface TreeNode<T> {
-    data: T;
-    children?: TreeNode<T>[];
-    expanded?: boolean;
-}
-
-interface FSEntry {
-    name: string;
-    size: string;
-    used: string;
-    kind: string;
-    items?: number;
-}
+import * as moment from 'moment';
 
 @Component({
     selector: 'ngx-kib',
     templateUrl: './kib.component.html',
     styleUrls: ['./kib.component.scss'],
 })
-export class KibComponent {
-    columns: [
-        {
-            name: 'productCode';
-            header: 'Название';
-        },
-        {
-            name: 'limit';
-            header: 'Лимит';
-        }
-    ];
-    data = [
-        {
-            data: {
-                id: '444e53e1-4e99-4496-a062-d58374cbf45d',
-                createdAt: '2023-01-05T09:47:29.55643Z',
-                productCode: 'Charmander',
-                currency: 'Kgs',
-                limit: 10000.0,
-            },
-            children: [
-                {
-                    data: {
-                        id: 'd15b66e5-d9d1-42ef-995b-4e5ff0f29b04',
-                        createdAt: '2023-01-05T09:53:20.389453Z',
-                        amount: 1000.0,
-                        contractorId: 1,
-                        contractorName: 'Sample Contractor Name',
-                        description: 'Sample text',
-                        repaymentDates: [
-                            {
-                                id: '70391077-fed9-4680-a097-11a3913db447',
-                                createdAt: '2023-01-05T09:53:20.38962Z',
-                                amount: 500.0,
-                                date: '2023-01-09T00:00:00Z',
-                                repayments: [],
-                                data: {},
-                            },
-                            {
-                                id: '8932b20f-d7e6-48a9-b011-e5b0ffb29bed',
-                                createdAt: '2023-01-05T09:53:20.389466Z',
-                                amount: 500.0,
-                                date: '2023-01-04T00:00:00Z',
-                                repayments: [],
-                                data: {},
-                            },
-                        ],
-                    },
-                },
-            ],
-        },
-    ];
-    // data = [
-    //     {
-    //         id: '444e53e1-4e99-4496-a062-d58374cbf45d',
-    //         createdAt: '2023-01-05T09:47:29.55643Z',
-    //         productCode: 'Charmander',
-    //         currency: 'Kgs',
-    //         limit: 10000.0,
-    //         activeContracts: [
-    //             {
-    //                 id: 'd15b66e5-d9d1-42ef-995b-4e5ff0f29b04',
-    //                 createdAt: '2023-01-05T09:53:20.389453Z',
-    //                 amount: 1000.0,
-    //                 contractorId: 1,
-    //                 contractorName: 'Sample Contractor Name',
-    //                 description: 'Sample text',
-    //                 repaymentDates: [
-    //                     {
-    //                         id: '70391077-fed9-4680-a097-11a3913db447',
-    //                         createdAt: '2023-01-05T09:53:20.38962Z',
-    //                         amount: 500.0,
-    //                         date: '2023-01-09T00:00:00Z',
-    //                         repayments: [],
-    //                         data: {},
-    //                     },
-    //                     {
-    //                         id: '8932b20f-d7e6-48a9-b011-e5b0ffb29bed',
-    //                         createdAt: '2023-01-05T09:53:20.389466Z',
-    //                         amount: 500.0,
-    //                         date: '2023-01-04T00:00:00Z',
-    //                         repayments: [],
-    //                         data: {},
-    //                     },
-    //                 ],
-    //                 data: {},
-    //             },
-    //         ],
-    //         data: {
-    //             RepaymentDay: '12',
-    //         },
-    //     },
-    // ];
-    // data: TreeNode<FSEntry>[] = [
-    //     {
-    //         data: {
-    //             name: 'Руслан Шоп',
-    //             size: '1.12.2022',
-    //             used: '6000',
-    //             items: 5,
-    //             kind: 'dir',
-    //         },
-    //         children: [
-    //             {
-    //                 data: {
-    //                     name: '1-й платеж',
-    //                     kind: 'dir',
-    //                     size: '2.12.2022',
-    //                     used: '1000',
-    //                 },
-    //                 children: [
-    //                     {
-    //                         data: {
-    //                             name: '1.12.2022',
-    //                             kind: 'dir',
-    //                             size: '',
-    //                             used: '3000',
-    //                         },
-    //                     },
-    //                 ],
-    //             },
-    //         ],
-    //     },
-    // ];
-}
+export class KibComponent implements OnInit {
+    @Input() data;
 
+    constructor() {}
+    getUsedMoney(data) {
+        let sum = 0;
+        data.map((item) => {
+            sum += item.amount;
+        });
+        return sum;
+    }
+    getOverdueDays(data) {
+        const amount = data.amount;
+        let repAmount = 0;
+        let days;
+        data.repayments.map((item) => {
+            repAmount += item.amount;
+        });
+
+        if (amount > repAmount || data.repayments.length === 0) {
+            const current_date = moment().format('YYYY-MM-DD');
+            days = +moment(data.date)
+                .add(-1, 'days')
+                .diff(current_date, 'days')
+                .toString()
+                .substring(1);
+        }
+
+        return days ? days : false;
+    }
+    ngOnInit() {}
+}
 @Component({
     selector: 'ngx-fs-icon',
     template: `
